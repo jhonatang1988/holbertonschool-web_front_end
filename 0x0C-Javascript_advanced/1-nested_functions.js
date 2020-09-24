b9b035b22fd8984ce47dd3f17fe6dcd792dfff19
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-09-23 22:45:39 -0500

    1

---
+++ 0x0C-Javascript_advanced/1-nested_functions.js
@@ -0,0 +1,17 @@
+const globalVariable = 'welcome '
+
+function outer () {
+  window.alert(globalVariable)
+  const course = 'Holberton'
+  function inner () {
+    window.alert(globalVariable + course)
+    const exclamation = '!'
+    function inception () {
+      window.alert(globalVariable + course + exclamation)
+    }
+    inception()
+  }
+  inner()
+}
+
+outer()
